b25d8eb5fa900dde08665283fd0d285f278595af
OEncouracado <tryironcl4d@gmail.com>
2025-08-05 10:43:19 -0300

    listar clientes

--- src/app/(private)/cliente/page.tsx
+++ src/app/(private)/cliente/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import CadastrarClientesForm from "@/components/(Clientes)/CadastrarClientesForm";
-import ListarClientes from "@/components/ListarClientes";
 import ListarClientes2 from "@/components/(Clientes)/ListarClientes2";
 
 export default function Client() {
   return (
     <>
       <ListarClientes2 />
-      {/* <CadastrarClientesForm /> */}
     </>
   );
 }
